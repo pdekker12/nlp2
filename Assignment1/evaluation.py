67bd81df83528db39d4f92aa59f716c12aaa1dc1
Minh Ngo <nlminhtl@gmail.com>
2015-04-23 11:56:47 +0200

    Fixing Log-Likelihood

--- Assignment1/evaluation.py
+++ Assignment1/evaluation.py
@@ -4,4 +4,4 @@ def compute_perplexity(translation_probs):
     return -sum(math.log2(prob) for prob in translation_probs)
 
 def compute_log_likelihood(translation_probs):
-    return -sum(math.log(prob) for prob in translation_probs)
+    return sum(math.log(prob) for prob in translation_probs)
