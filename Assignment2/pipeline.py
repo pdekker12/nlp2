b47fb6e27f8c33700e6850626909f34cf5cdfab0
pdekker12 <peter.dekker@student.uva.nl>
2015-05-13 13:14:00 +0200

    workflow

---
+++ Assignment2/pipeline.py
@@ -0,0 +1,9 @@
+
+# Load one/multiple parallel corpora
+# Run different POS taggers on the source sides of the parallel corpora
+# Align every parallel corpus
+
+# Map source POS tags to target POS tags using alignment. Combine multiple tagged corpora. Use smoothing.
+
+# Evaluate the target tags using annotated corpus.
+
