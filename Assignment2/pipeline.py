6bdd4878686dbc5a002f438f5137bb27f3fe5c87
Minh Ngo <nlminhtl@gmail.com>
2015-05-17 20:29:42 +0200

    Specifying the python version. Removing whitespaces.

--- Assignment2/pipeline.py
+++ Assignment2/pipeline.py
@@ -1,25 +1,28 @@
-from nltk.tag import pos_tag 
-from nltk.tokenize import word_tokenize
+#!/usr/bin/env python2
+
 import sys
 import subprocess
 import os
+
+from nltk.tag import pos_tag
+from nltk.tokenize import word_tokenize
 
 corpus_paths = ["data/en-cs-combined10000.txt"]
 
 def main():
     # Previous steps done by other programs
-        # Load one/multiple parallel corpora
-        # Align every parallel corpus
-    
-    
+    # Load one/multiple parallel corpora
+    # Align every parallel corpus
+
+
     for corpus_path in corpus_paths:
         # Perform alignment
         command = "./fast_align/fast_align"
         output = subprocess.check_output([command, "-i", corpus_path])
         print "output"
         alignment_list = output.split("\n")[:-1]
-        
-                
+
+
         # Open corpus
         corpus_file = open(corpus_path,"r")
         corpus = corpus_file.readlines()
@@ -34,7 +37,7 @@ def main():
             print source_words
             source_tags = pos_tag(source_words)
             print source_tags
-            
+
             # Map source POS tags to target POS tags using alignment.
             # TODO: Combine multiple tagged corpora. Use smoothing.
             # Get alignments for this line
@@ -49,7 +52,7 @@ def main():
             print target_words
             print target_tags
 
-            
+
 
         # Evaluate the target tags using annotated corpus.
 
