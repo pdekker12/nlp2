ac7336cc936aaf5834e4485f5c803b3cd074b9d7
Minh Ngo <nlminhtl@gmail.com>
2015-05-27 12:00:18 +0200

    A bit more refactoring

--- Assignment2/pipeline.py
+++ Assignment2/pipeline.py
@@ -98,6 +98,83 @@ def create_stanford_postagger():
                      'stanford-postagger-2015-04-20/stanford-postagger.jar')
 
 
+def pos_score(corpus_path, tagger):
+    wordtag_1to1_prob = {}
+    wordtag_1toN_prob = {}
+    word_count_1to1 = {}
+    word_count_1toN = {}
+    word_count = {}
+    pos_count = {}
+
+    with open(corpus_path, 'r') as corpus_file:
+        for (source_words, target_words, source_tags), alignments in zip(parse_corpus(corpus_file, tagger),
+                                                                         mt_alignment(corpus_path)):
+            link_count = [0] * len(source_words)
+            for source_id, _ in alignments:
+                link_count[source_id] += 1
+            one_to_n_marker = [count > 1 for count in link_count]
+
+            for (source_ind, _), target_word in zip(alignments, target_words):
+                pos_tag = source_tags[source_ind][1]
+                key = (target_word, pos_tag)
+                increase(word_count, target_word)
+                increase(pos_count, pos_tag)
+                if one_to_n_marker[source_ind]:
+                    increase(word_count_1toN, target_word)
+                    increase(wordtag_1toN_prob, key)
+                else:
+                    increase(word_count_1to1, target_word)
+                    increase(wordtag_1to1_prob, key)
+
+    for key in wordtag_1to1_prob:
+        wordtag_1to1_prob[key] /= word_count_1to1[key[0]]
+
+    for key in wordtag_1toN_prob:
+        wordtag_1toN_prob[key] /= word_count_1toN[key[0]]
+
+    del word_count_1to1
+    del word_count_1toN
+
+    word_norm_coef = sum(word_count.values())
+    word_count = {word : count / word_norm_coef for word, count in word_count.items()}
+
+    pos_norm_coef = sum(pos_count.values())
+    pos_count = {pos_tag : count / pos_norm_coef for pos_tag, count in pos_count.items()}
+
+    wordtag_score = {}
+
+    uniq_keys = set(wordtag_1to1_prob.keys()).union(wordtag_1toN_prob.keys())
+    for key in uniq_keys:
+        if key in wordtag_1to1_prob:
+            score = wordtag_1to1_prob[key]
+            if key in wordtag_1toN_prob:
+                score += wordtag_1toN_prob[key]
+                score /= 2.0
+            wordtag_score[key] = score
+        else:
+            wordtag_score[key] = wordtag_1toN_prob[key]
+    print('WordTag score size:', len(wordtag_score))
+
+    del wordtag_1to1_prob
+    del wordtag_1toN_prob
+
+    word_to_tags = {}
+    for (word, tag), score in wordtag_score.items():
+        if word in word_to_tags:
+            word_to_tags[word].append((tag, score))
+        else:
+            word_to_tags[word] = [(tag, score)]
+
+    wordtag_score = {}
+    for word, tags in word_to_tags.items():
+        toptwo = heapq.nlargest(2, tags, lambda x: x[1])
+        norm = reduce(lambda x, y: x + y[1] , toptwo, 0)
+        for tag, score in toptwo:
+            wordtag_score[(word, tag)] = score / norm
+
+    return wordtag_score
+
+
 def main():
     # Previous steps done by other programs
     # Load one/multiple parallel corpora
@@ -110,86 +187,8 @@ def main():
     print('POS tagger created!')
 
     for corpus_path in corpus_paths:
-        corpus_path_id = corpus_paths.index(corpus_path)
-
-        wordtag_1to1_prob = {}
-        wordtag_1toN_prob = {}
-        word_count_1to1 = {}
-        word_count_1toN = {}
-        word_count = {}
-        pos_count = {}
-
-        print('Openning the corpus', corpus_path)
-        # Open corpus
-        with open(corpus_path, 'r') as corpus_file:
-            for (source_words, target_words, source_tags), alignments in zip(parse_corpus(corpus_file, tagger), mt_alignment(corpus_path)):
-
-                link_count = [0] * len(source_words)
-                for source_id, _ in alignments:
-                    link_count[source_id] += 1
-                one_to_n_marker = [count > 1 for count in link_count]
-
-                for (source_ind, _), target_word in zip(alignments, target_words):
-                    pos_tag = source_tags[source_ind][1]
-                    key = (target_word, pos_tag)
-                    increase(word_count, target_word)
-                    increase(pos_count, pos_tag)
-                    if one_to_n_marker[source_ind]:
-                        increase(word_count_1toN, target_word)
-                        increase(wordtag_1toN_prob, key)
-                    else:
-                        increase(word_count_1to1, target_word)
-                        increase(wordtag_1to1_prob, key)
-
-        for key in wordtag_1to1_prob:
-            wordtag_1to1_prob[key] /= word_count_1to1[key[0]]
-        print('wordtag_1to1_prob size:', len(wordtag_1to1_prob))
-
-        for key in wordtag_1toN_prob:
-            wordtag_1toN_prob[key] /= word_count_1toN[key[0]]
-        print('wordtag_1toN_prob size:', len(wordtag_1toN_prob))
-
-        word_norm_coef = sum(word_count.values())
-        word_count = {word : count / word_norm_coef for word, count in word_count.items()}
-
-        pos_norm_coef = sum(pos_count.values())
-        pos_count = {pos_tag : count / pos_norm_coef for pos_tag, count in pos_count.items()}
-
-        del word_count_1to1
-        del word_count_1toN
-
-        wordtag_score = {}
-
-        uniq_keys = set(wordtag_1to1_prob.keys()).union(wordtag_1toN_prob.keys())
-        for key in uniq_keys:
-            if key in wordtag_1to1_prob:
-                score = wordtag_1to1_prob[key]
-                if key in wordtag_1toN_prob:
-                    score += wordtag_1toN_prob[key]
-                    score /= 2.0
-                wordtag_score[key] = score
-            else:
-                wordtag_score[key] = wordtag_1toN_prob[key]
-        print('WordTag score size:', len(wordtag_score))
-
-        del wordtag_1to1_prob
-        del wordtag_1toN_prob
-
-        word_to_tags = {}
-        for (word, tag), score in wordtag_score.items():
-            if word in word_to_tags:
-                word_to_tags[word].append((tag, score))
-            else:
-                word_to_tags[word] = [(tag, score)]
-
-        wordtag_score = {}
-        for word, tags in word_to_tags.items():
-            toptwo = heapq.nlargest(2, tags, lambda x: x[1])
-            norm = reduce(lambda x, y: x + y[1] , toptwo, 0)
-            for tag, score in toptwo:
-                wordtag_score[(word, tag)] = score / norm
-        pprint.pprint(wordtag_score)
-
+        score = pos_score(corpus_path, tagger)
+        pprint.pprint(score)
         # TODO Combine multiple tagged corpora
 
         # TODO: Evaluate the target tags using annotated corpus.
