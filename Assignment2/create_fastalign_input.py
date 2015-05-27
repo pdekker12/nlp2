ff8005c451471fd138330847746246a62747224a
Minh Ngo <nlminhtl@gmail.com>
2015-05-27 12:23:50 +0200

    Removing punctuations

--- Assignment2/create_fastalign_input.py
+++ Assignment2/create_fastalign_input.py
@@ -4,6 +4,7 @@
 # Usage: create_fastalign_input.py SOURCE_PATH TARGET_PATH
 
 import sys
+import string
 from nltk.tokenize import word_tokenize
 
 if len(sys.argv) < 3:
@@ -18,6 +19,9 @@ target_file = open(target_path, 'r')
 for source_sentence, target_sentence in zip(source_file, target_file):
     source_tokens = word_tokenize(source_sentence)
     target_tokens = word_tokenize(target_sentence)
+
+    source_tokens = [w for w in source_tokens if w not in string.punctuation]
+    target_tokens = [w for w in target_tokens if w not in string.punctuation]
     if len(source_tokens) > 0 and len(target_tokens) > 0:
         source_sentence = ' '.join(source_tokens)
         target_sentence = ' '.join(target_tokens)
