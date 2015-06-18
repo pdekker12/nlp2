b2b88019661d5c27a5d85ed5efe86c5ea733e243
Minh Ngo <nlminhtl@gmail.com>
2015-06-19 00:22:25 +0200

    Initializing core_tags once

--- Assignment2/pos.py
+++ Assignment2/pos.py
@@ -22,8 +22,8 @@ for language in f:
         else:
             core_to_generic_dict[language][value].add(key)
 
-    core_tags_without_start = list(core_to_generic_dict[language].keys())
-    core_tags = core_tags_without_start + ['$']
+core_tags_without_start = list(next(iter(core_to_generic_dict.values())).keys())
+core_tags = core_tags_without_start + ['$']
 
 for lang_file in f.values():
     lang_file.close()
